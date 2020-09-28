68eeda76b13b3e347cdae1e1fab2103a2ea4ea5d
pratibha.h <pratibha.h@innovify.in>
2020-09-28 21:13:26 +0530

    api changes

--- server.js
+++ server.js
@@ -22,5 +22,8 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'))
+    app.use(express.static('./client/build'))
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, './', 'client', 'build', 'index.html'));
+    });
 }
